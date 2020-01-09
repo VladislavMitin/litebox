394cde820af80040665c7839986c88c0df261224
Vladislav <vladislav.mitin1993@gmail.com>
2020-01-09 17:39:59 +0300

    Remove constants

--- app/src/main/java/ru/mitin/vladislav/litebox/core/Constants.java
+++ app/src/main/java/ru/mitin/vladislav/litebox/core/Constants.java
@@ -1,6 +1,6 @@
 package ru.mitin.vladislav.litebox.core;
 
 public class Constants {
-    public static final String GOOGLE_API_KEY = "AIzaSyDkxpGDeRH2FToIzulMBuxP4N_svVntK0U";
-    public static final String GOOGLE_CX = "017576662512468239146:omuauf_lfve";
+    public static final String GOOGLE_API_KEY = "";
+    public static final String GOOGLE_CX = "";
 }
